f87ab1f2d93aec2eb72d3df8496443a132d79cc8
Hongyang Zhou <hyzhou@umich.edu>
2021-03-02 13:31:41 +0200

    One simple test for now

---
+++ testpackage/tests/advection1d/advection1d.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Run the test and compare the result.
+
+cd $(dirname "$0")
+
+# Compared filename
+filecheck="bulk.0000001.vlsv"
+
+# Number of MPI processes
+nprocs=1
+# Number of OpenMP threads
+nthreads=1
+# Tolerance that scales with quantity magnitude
+tol=1e-8
+
+source ../../run_and_check.sh $PWD
