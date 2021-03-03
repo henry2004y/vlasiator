62a93e3e13dcd4508e607b66368ba914baba51ac
Hongyang Zhou <hyzhou@umich.edu>
2021-03-03 20:02:25 +0200

    add test script

---
+++ testpackage/tests/advection2d/advection2d.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Run the test and compare the result.
+
+cd $(dirname "$0")
+
+# Compared filename
+filecheck="bulk.0000005.vlsv"
+
+# Number of MPI processes
+nprocs=1
+# Number of OpenMP threads
+nthreads=1
+# Tolerance that scales with quantity magnitude
+tol=1e-4
+
+source ../../run_and_check.sh $PWD
